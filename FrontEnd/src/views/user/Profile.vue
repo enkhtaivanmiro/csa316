ea2386d4629d4688ec2b071b33c5f956bf5705da
Anka <zanhaa56@gmail.com>
2025-10-13 02:27:27 +0800

    user info section done

--- FrontEnd/src/views/user/Profile.vue
+++ FrontEnd/src/views/user/Profile.vue
@@ -1,17 +1,40 @@
 <template>
     <Background>
         <Navbar />
-        <div>
+        <div class="container">
             <div class="user-container">
                 <div class="user-pic">
                     <img src="/Landing Page/User.svg" alt="pic" class="user-dummy">
-                    <p>{{ user }}</p>
+                    <p id="username">{{ user }}</p>
                 </div>
-                <div class="navigation-container">
-                    
+                <ul class="navigation-container">
+                    <li>
+                        <RouterLink to="/myprojects">Миний мэдээлэл</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/myprojects">Миний төслүүд</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/myprojects">Миний санхүү</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/myprojects">Төсөл оруулах</RouterLink>
+                    </li>
+                    <li>
+                        <button @click="logout" class="logout">Гарах</button>
+                    </li>
+                </ul>
+            </div>
+            <div class="info-container">
+                <div>
+                    <p class="text">Хэрэглэгчийн нэр</p>
+                    <p class="info">{{ user }}</p>
+                </div>
+                <div>
+                    <p class="text">Цахим шуудан</p>
+                    <p class="info">{{ email }}</p>
                 </div>
             </div>
-            <div class="info-container"></div>
         </div>
         <Footer />
     </Background>
@@ -19,15 +42,18 @@
 <script setup>
 import { ref, inject, onBeforeMount } from 'vue'
 import { jwtDecode } from 'jwt-decode';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 import Background from '../components/background.vue';
 import Navbar from '../components/navbar.vue';
 import Footer from '../components/footer.vue';
 
 const api = inject('api')
-const user = ref(null)
 const route = useRoute()
+const router = useRouter()
+
+const user = ref(null)
+const email = ref(null)
 
 const token = localStorage.getItem('authToken')
 
@@ -42,26 +68,88 @@ onBeforeMount(async () => {
         const res = await api.get(`/users/${userID}`)
 
         user.value = res.data.username
-    }catch(e){
+        email.value = res.data.email
+    } catch (e) {
         console.error('error fetching: ', e)
     }
 })
+
+function logout(){
+    localStorage.removeItem('authToken')
+    router.push('/')
+}
 </script>
 
 <style scoped>
-.user-pic{
+#username {
+    font-size: 1.5rem;
+}
+
+.text {
+    color: var(--secondary-text);
+    font-size: 0.875rem;
+}
+
+.container {
+    display: flex;
+    margin: 50px 60px;
+    gap: 50px;
+}
+
+.user-container {
     display: flex;
     flex-direction: column;
+    gap: 2.5rem;
+}
+
+.user-pic {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
     max-width: 220px;
     border-radius: 28px;
-    border: 1px solid var(--secondary-text);
     background-color: var(--background-color);
+    border: 1px solid var(--secondary-text);
     text-align: center;
-
+    padding: 1.5rem 0;
     justify-content: center;
-    align-items: center;  
+    align-items: center;
 }
-.user-dummy{
+
+.user-dummy {
     max-width: 150px;
+}
+
+.navigation-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    background-color: var(--background-color);
+    max-width: 220px;
+    border-radius: 28px;
+    padding: 20px 20px;
+    border: 1px solid var(--secondary-text);
+}
+
+.navigation-container a {
+    text-decoration: none;
+    color: var(--primary-text);
+}
+
+.navigation-container .logout {
+    background: none;
+    border: none;
+    color: var(--primary-text);
+    font: inherit;
+    cursor: pointer;
+    text-align: left;
+}
+
+.info-container {
+    background-color: var(--background-color);
+    border: 1px solid var(--secondary-text);
+    flex: 1;
+    border-radius: 28px;
+    padding: 30px 30px;
 }
 </style>
